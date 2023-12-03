f331ee7fb58bdac1c11ec8e8b8cb909abb26db6f
Bhumir2503 <bhumir2503@gmail.com>
2023-12-03 11:25:29 -0500

    custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Website-Resume-V2/",
+  // base: "/Website-Resume-V2/",
   plugins: [react()],
 })
